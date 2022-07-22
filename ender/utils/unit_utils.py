65750f198faa78ca192839b8403c8a73e0b10c15
auto-formatter <auto-formatter@users.noreply.github.com>
2022-07-22 11:18:02 +0000

    Fix: Code Format with Black

--- ender/utils/unit_utils.py
+++ ender/utils/unit_utils.py
@@ -16,4 +16,3 @@ def range_vs(unit: Unit, target: Unit) -> float:
 def calculate_combat_value(bot_ai: BotAI, unit: Unit) -> float:
     unit_value = bot_ai.calculate_unit_value(unit.type_id)
     return unit.shield_health_percentage * (unit_value.minerals + unit_value.vespene)
-    
